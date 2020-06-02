8897286f20b82e869bd5bc381491b945fcc1ff61
Gfragata <gfragata97@gmail.com>
2020-06-01 22:04:59 -0300

    Primeiro módulo node

---
+++ aula 1/app.js
@@ -0,0 +1,5 @@
+const modulo1 = require('./mod1');
+const modulo2 = require('./mod2');
+
+modulo1('meu primeiro modulo');
+modulo2.mensagem('meu segundo modulo');
---
+++ aula 1/mod2.js
@@ -0,0 +1,11 @@
+exports.mensagem = function(parametro){
+    console.log(parametro);
+}
+
+exports.mensagem2 = function(parametro){
+    console.log(parametro);
+}
+
+exports.mensagem3 = function(parametro){
+    console.log(parametro);
+}
